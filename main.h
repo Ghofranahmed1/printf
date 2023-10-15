ab606a1c4bf12dd76d11156b9a832336c2d2e54d
Munir405 <deelagbrainy2@gmail.com>
2023-10-15 06:54:47 -0700

    header file

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * func_Array - structure that holds format conversion specifiers.
+ * @holder - a format placeholder.
+ * @*f_ptr - a function pointer.
+ */
+
+typedef struct func_Array
+{
+	char holder;
+	int (*f_ptr)(va_list, int);
+} func;
+
+int _putchar(char c);
+int (*read_func(char c))(va_list, int);
+int _printf(const char *format, ...);
+
+		
