88a312ff004e90d340bcbdc76587afc47b91b03d
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-15 14:27:41 -0700

    thired try

--- _printf.c
+++ _printf.c
@@ -1,52 +1,38 @@
 #include "main.h"
-/**
- * _printf - function produces according to a format
- * @format: conversin specifiers
- * Return: number of char printed
- */
 
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int char_print = 0;
-	va_list list_args;
+	va_list args;
+        int char_print = 0;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
-	va_start(list_args, format);
+	va_start(args, format);
 	while (*format)
 	{
-		if (*format != '%')
+		if (*format == '%' && format[1] == '%')
+		{
+			write(1, "%", 1);
+			char_print += 1;
+			format += 2;
+		}
+		else if (*format == '%')
+		{
+			char_print += handle_format(format + 1, args);
+			format++;
+		}
+		else
 		{
 			write(1, format, 1);
 			char_print++;
 		}
-		else if (*format == '%' && format[1] == '%')
-		{
-			write (1, "%%", 2);
-			char_print += 2;
-			format++;
-		}
-			else if (*format == 'c' || *format == 's')
-			{
-				if (*format == 'c')
-				{
-					char c = va_arg(args, int)
-				write(1, &c, 1);
-				char_print++;
-				}
-				else if (*format == 's')
-				{
-					char *str = va_arg(list_args, char*);
-					int len1 = 0;
-					while (str[len1] != '\0')
-                                               len1++;
-                                write(1, str, len1);
-                                char_print += len1;
-				}
-			}
-		}
 		format++;
 	}
-	va_end(list_args);
+	va_end(args);
 	return (char_print);
 }
