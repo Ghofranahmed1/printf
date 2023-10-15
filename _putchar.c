a9e7dee13751534d1436a476b06721cfb9e5b123
Munir405 <deelagbrainy2@gmail.com>
2023-10-15 07:00:14 -0700

    Print the char to stdout

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#incude "main.h"
+
+/**
+ * _putchar - A function that print 
+ * a single character at a time
+ * @c - the argument to return
+ * Return: the argument, Always (1) success
+ */
+
+int _putchar(char c)
+{
+	return (writ(1, &c, 1));
+}
