f650541a7d78991aeb8374e3855e30e3edc82a78
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-10-17 07:33:38 -0700

    i and d function

---
+++ handle_format_i_d.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * handle_format_i_d - fun thi print intger and 
+ * @format: specifire list
+ * @args: list contain the vat to pull
+ * Return: number of char the printed
+ */
+
+int handle_format_i_d( const char * format, va_list args)
+{
+	int num, num_len, count = 0;
+	char num_str[12];
+
+	if (*format == 'd' || *format == 'i')
+	{
+		num = va_arg(args, int);
+		itoa(num, num_str);
+		while (num_str[num_len] != '\0')
+			num_len++;
+		write(1, num_str, num_len);
+		count += num_len;
+	}
+return (count);
+}
