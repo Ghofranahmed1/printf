4f36d219cbaa77497e7eaf1b1dc76e591d0c44a4
Munir405 <deelagbrainy2@gmail.com>
2023-10-22 11:21:31 -0700

    Print the octal *_*

--- printf_oct.c
+++ printf_oct.c
@@ -1,35 +1,17 @@
 #include "main.h"
 
 /**
- * printf_oct - prints an octal number.
- * @val: arguments.
- * Return: counter.
+ * print_octal - Prints octal notation of a number
+ * @list: List of arguments
+ * @length: Current length of string
+ * Return: The length
  */
-int printf_oct(va_list val)
+
+int print_octal(va_list list, int length)
 {
-	int i;
-	int *array;
-	int counter = 0;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int temp = num;
+	unsigned int n = va_arg(list, unsigned int);
 
-	while (num / 8 != 0)
-	{
-		num /= 8;
-		counter++;
-	}
-	counter++;
-	array = malloc(counter * sizeof(int));
+	length += print_numbers(n, 8, "01234567");
 
-	for (i = 0; i < counter; i++)
-	{
-		array[i] = temp % 8;
-		temp /= 8;
-	}
-	for (i = counter - 1; i >= 0; i--)
-	{
-		_putchar(array[i] + '0');
-	}
-	free(array);
-	return (counter);
+	return (length);
 }
